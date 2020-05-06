442e03048b824f1d8c8ead3571d45185625b0b58
Jerome Baude <jerome@hashicorp.com>
2020-05-06 12:09:08 +0200

    adding new vnet

---
+++ newvnet.tf
@@ -0,0 +1,8 @@
+//--------------------------------------------------------------------
+// Modules
+module "vnet" {
+  source  = "app.terraform.io/jerome-playground/vnet/azurerm"
+  version = "2.0.0"
+
+  resource_group_name = "jerome-workshop"
+}
