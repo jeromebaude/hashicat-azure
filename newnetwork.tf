d230129ecc5c320b5304915fb6b55ded355a8889
Jérôme Baude <jerome.baude@gmail.com>
2020-09-11 10:48:24 +0200

    Delete newnetwork.tf

--- newnetwork.tf
+++
@@ -1,8 +0,0 @@
-//--------------------------------------------------------------------
-// Modules
-module "network" {
-  source  = "app.terraform.io/jerome-playground/network/azurerm"
-  version = "3.0.1"
-
-  resource_group_name = "jerome-workshop"
-}
