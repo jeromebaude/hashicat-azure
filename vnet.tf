6a2bdba94b6e224891d47534b1ce9a97c083efa0
jbaude <jbaude@nivura.local>
2021-02-15 17:00:50 +0100

    network

--- vnet.tf
+++ vnet.tf
@@ -1,6 +1,6 @@
-module "vnet" {
-  source  = "app.terraform.io/jerome-playground/vnet/azurerm"
-  version = "2.0.0"
+module "network" {
+  source  = "app.terraform.io/jerome-playground/network/azurerm"
+  version = "3.0.1"
   # insert required variables here
   resource_group_name = azurerm_resource_group.myresourcegroup.name
 }
