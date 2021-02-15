ad3181f97d3b5ae19fabb86f5d655fcc818452de
jbaude <jbaude@nivura.local>
2021-02-15 16:46:32 +0100

    vnet

--- vnet.tf
+++ vnet.tf
@@ -2,4 +2,5 @@ module "vnet" {
   source  = "app.terraform.io/jerome-playground/vnet/azurerm"
   version = "2.0.0"
   # insert required variables here
+  resource_group_name = azurerm_resource_group.myresourcegroup.name
 }
